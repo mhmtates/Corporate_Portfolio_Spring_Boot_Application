228124115da89383323dc0d7d80d1ca7fc9e20e5
Mehmet Ateş Özateş <ozatesatess@gmail.com>
2026-04-09 05:41:55 +0300

    All project files added

---
+++ src/main/java/com/mehmetatesozates/CorporatePortfolioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mehmetatesozates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporatePortfolioSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporatePortfolioSpringBootApplication.class, args);
+    }
+
+}
